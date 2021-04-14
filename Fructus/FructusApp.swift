4ea64102f89b18dce673c5e24ec9c4fe6379d773
amir-sediqi69 <amir.sediqi69@gmail.com>
2021-04-14 16:04:01 +0200

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Amir Sediqi on 2021-04-14.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
