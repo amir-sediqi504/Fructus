71a1d21bd3dc754d44b0051219417aff7be5e807
amir-sediqi69 <amir.sediqi69@gmail.com>
2021-04-20 13:10:29 +0200

    fetching data

--- Fructus/App/OnboardingView.swift
+++ Fructus/App/OnboardingView.swift
@@ -8,23 +8,28 @@
 import SwiftUI
 
 struct OnboardingView: View {
-    //:- PROPERTIES
-    
-    //:- Mark BODY
-    var body: some View {
-        TabView {
-            ForEach(0..<6){item in
-                FruitCardView()
-            }   //: loop to create pages
-        } //: TAB
-        .tabViewStyle(PageTabViewStyle())
-        .padding(.vertical, 10)
-    }
+  // MARK: - PROPERTIES
+  
+  var fruits: [Fruit] = fruitsData
+  
+  // MARK: - BODY
+  
+  var body: some View {
+    TabView {
+      ForEach(fruits[0...5]) { item in
+        FruitCardView(fruit: item)
+      } //: LOOP
+    } //: TAB
+    .tabViewStyle(PageTabViewStyle())
+    .padding(.vertical, 20)
+  }
 }
 
-//:- Mark Preview
+// MARK: - PREVIEW
+
 struct OnboardingView_Previews: PreviewProvider {
-    static var previews: some View {
-        OnboardingView()
-    }
+  static var previews: some View {
+    OnboardingView(fruits: fruitsData)
+      .previewDevice("iPhone 11 Pro")
+  }
 }
--- Fructus/View/FruitCardView.swift
+++ Fructus/View/FruitCardView.swift
@@ -8,51 +8,59 @@
 import SwiftUI
 
 struct FruitCardView: View {
-    // MARK: - PROPERTIES
-    @State private var isAnimating: Bool = false
-    
-    // MARK: - BODY
-    var body: some View {
-        ZStack {
-            VStack(spacing: 20) {
-                // Fruit: Image
-                Image("blueberry")
-                    .resizable()
-                    .scaledToFit()
-                    .shadow(color: .blue, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: 6, y: 8)
-                    .scaleEffect(isAnimating ? 1.0 : 0.6)
-                // Fruit: Title
-                Text("Blueberry")
-                    .foregroundColor(.white)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .shadow(color: .white, radius: 2, x: 2, y: 2)
-                // Fruit: Headline
-                    Text("Blueberries are sweet, nutritious and a very popular fruit all over the world.")
-                        .foregroundColor(Color.white)
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal, 18)
-                        .frame(maxWidth: 480)
-                //Button: Start
-                StartButtonView()
-            } //: Vstack
-        } //: ZSTACK
-        .onAppear(){
-            withAnimation(.easeOut(duration: 0.5)){
-                isAnimating = true
-            }
-        }
-        .frame(minWidth: 0 , maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-        .background(LinearGradient(gradient: Gradient(colors: [Color("BlueberryLight"),
-            Color("BlueberyyDark")]), startPoint: .top, endPoint: .bottom))
-        .cornerRadius(25)
-        .padding(.horizontal, 15)
+  // MARK: - PROPERTIES
+  
+  var fruit: Fruit
+  
+  @State private var isAnimating: Bool = false
+  
+  // MARK: - BODY
+  
+  var body: some View {
+    ZStack {
+      VStack(spacing: 20) {
+        // FRUIT: IMAGE
+        Image(fruit.image)
+          .resizable()
+          .scaledToFit()
+          .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+          .scaleEffect(isAnimating ? 1.0 : 0.6)
+        
+        // FRUIT: TITLE
+        Text(fruit.title)
+          .foregroundColor(Color.white)
+          .font(.largeTitle)
+          .fontWeight(.heavy)
+          .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
+        
+        // FRUIT: HEADLINE
+        Text(fruit.headline)
+          .foregroundColor(Color.white)
+          .multilineTextAlignment(.center)
+          .padding(.horizontal, 16)
+          .frame(maxWidth: 480)
+        
+        // BUTTON: START
+        StartButtonView()
+      } //: VSTACK
+    } //: ZSTACK
+    .onAppear {
+      withAnimation(.easeOut(duration: 0.5)) {
+        isAnimating = true
+      }
     }
+    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+    .background(LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom))
+    .cornerRadius(20)
+    .padding(.horizontal, 20)
+  }
 }
+
 // MARK: - PREVIEW
+
 struct FruitCardView_Previews: PreviewProvider {
-    static var previews: some View {
-        FruitCardView()
-            .previewLayout(.fixed(width: 320, height: 640))
-    }
+  static var previews: some View {
+    FruitCardView(fruit: fruitsData[1])
+      .previewLayout(.fixed(width: 320, height: 640))
+  }
 }
