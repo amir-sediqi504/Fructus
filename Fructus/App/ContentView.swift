c0737a159237c38a2100b99ad34f6c314f39a99c
amir-sediqi69 <amir.sediqi69@gmail.com>
2021-04-21 13:18:57 +0200

    List View

--- Fructus/App/ContentView.swift
+++ Fructus/App/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - Properties
+    var fruits: [Fruit] = fruitsData
+    //: - BODY
     var body: some View {
-        Text("Hello, Fruit!")
-            .padding()
+        NavigationView{
+            List{
+                ForEach(fruits.shuffled()){
+                    item in FruitRowView(fruit: item)
+                }
+            }
+            .navigationTitle("Fruits")
+        }//: Navigation
+        .padding(.vertical, 4)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(fruits: fruitsData)
     }
 }
